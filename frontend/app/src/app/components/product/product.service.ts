6ea1568ea6209159db22744a712e4b6d4029051e
devguilhermeribeiro <dev.guilhermeribeiro@gmail.com>
2022-07-17 11:54:47 -0300

    criado service do produto adicionado funcao create

---
+++ frontend/app/src/app/components/product/product.service.ts
@@ -0,0 +1,31 @@
+import { Product } from './product.module';
+import { Injectable } from "@angular/core";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from 'rxjs';
+
+@Injectable ({
+    providedIn: 'root'
+})
+
+export class ProductService {
+
+    baseUrl = "http://localhost:3001/products"
+
+    constructor(private snackBar: MatSnackBar, private httpClient: HttpClient) { }
+    
+    showMessage(msg: string): void {
+        this.snackBar.open(msg, 'X', {
+          duration: 3000,
+          horizontalPosition: "right",
+          verticalPosition: "top"
+        })
+    }
+
+    create(product: Product): Observable<Product> {
+        return this.httpClient.post<Product>(this.baseUrl, product);
+    }
+
+    
+
+}
