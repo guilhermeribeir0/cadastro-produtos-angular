13c5631e3fccb39c8ff92eb0cebe7f4597063414
devguilhermeribeiro <dev.guilhermeribeiro@gmail.com>
2022-07-20 21:21:54 -0300

    configurado rotas

--- frontend/app/src/app/app-routing.module.ts
+++ frontend/app/src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { HomeComponent } from './views/home/home.component';
+
+const routes: Routes = [{
+  path: "",
+  component: HomeComponent
+}, {
+  path: "products",
+  component: ProductCrudComponent
+},
+{
+  path: "product/create",
+  component: ProductCreateComponent
+},
+{
+  path: "product/update/:id",
+  component: ProductUpdateComponent
+},
+{
+  path: "product/delete/:id",
+  component: ProductDeleteComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
