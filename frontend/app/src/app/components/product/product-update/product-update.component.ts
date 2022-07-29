99e50474c16254b491deb802701dfdc76614fd05
devguilhermeribeiro <dev.guilhermeribeiro@gmail.com>
2022-07-29 11:03:55 -0300

    criado funcoes update e cancel no componente update

--- frontend/app/src/app/components/product/product-update/product-update.component.ts
+++ frontend/app/src/app/components/product/product-update/product-update.component.ts
@@ -1,3 +1,6 @@
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from './../product.service';
+import { Product } from './../product.module';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductUpdateComponent implements OnInit {
 
-  constructor() { }
+  product!: Product;
+
+  constructor(private productService: ProductService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.productService.readById(id!).subscribe(product => {
+      this.product = product;
+    })
+  }
+
+  updateProduct(): void {
+    this.productService.update(this.product).subscribe(() => {
+      this.productService.showMessage('Produto atualizado com sucesso!');
+      this.router.navigate(['/products']);
+    })
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products']);
   }
 
 }
