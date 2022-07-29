fb354959d441520eb5d1901d2962136cb3f414f4
devguilhermeribeiro <dev.guilhermeribeiro@gmail.com>
2022-07-29 11:29:59 -0300

    adicionado funcoes delete e cancel no componente delete

--- frontend/app/src/app/components/product/product-delete/product-delete.component.ts
+++ frontend/app/src/app/components/product/product-delete/product-delete.component.ts
@@ -1,3 +1,6 @@
+import { Product } from './../product.module';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ProductService } from './../product.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDeleteComponent implements OnInit {
 
-  constructor() { }
+  product!: Product;
+
+  constructor(private productService: ProductService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.productService.readById(id!).subscribe(product => {
+      this.product = product;
+    })
+  }
+
+  deleteProduct(): void {
+    this.productService.delete(this.product.id!).subscribe(() => {
+      this.productService.showMessage('Produto excluido com sucesso!');
+      this.router.navigate(['products']);
+    })
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products']);
   }
 
 }
