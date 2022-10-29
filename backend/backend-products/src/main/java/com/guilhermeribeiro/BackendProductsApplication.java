ed7c302e84bdd9ef297f4d9c5d4049b2324860b6
guilhermeribeir0 <dev.guilhermeribeiro@gmail.com>
2022-10-29 16:49:21 -0300

    ajustado controller e trocado porta

--- backend/backend-products/src/main/java/com/guilhermeribeiro/BackendProductsApplication.java
+++ backend/backend-products/src/main/java/com/guilhermeribeiro/BackendProductsApplication.java
@@ -1,13 +1,31 @@
 package com.guilhermeribeiro;
 
+import com.guilhermeribeiro.model.Product;
+import com.guilhermeribeiro.repository.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BackendProductsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendProductsApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner initDatabase(ProductRepository productRepository) {
+		return args -> {
+			productRepository.deleteAll();
+
+			Product product = new Product();
+			product.setName("Mouse Gamer");
+			product.setReference("HDJ584DG");
+			product.setPrice(1499.99);
+
+			productRepository.save(product);
+		};
 	}
 
 }
--- backend/backend-products/src/main/java/com/guilhermeribeiro/controller/ProductController.java
+++ backend/backend-products/src/main/java/com/guilhermeribeiro/controller/ProductController.java
@@ -3,18 +3,20 @@ package com.guilhermeribeiro.controller;
 import com.guilhermeribeiro.model.Product;
 import com.guilhermeribeiro.repository.ProductRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "api/products")
+@RequestMapping(value = "/api/products")
 @AllArgsConstructor
 public class ProductController {
 
     private final ProductRepository productRepository;
 
+    @GetMapping
     public List<Product> productList() {
         return productRepository.findAll();
     }
