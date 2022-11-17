2af30fd93dd90149a45e19a0503b2107d22337ca
guilhermeribeir0 <dev.guilhermeribeiro@gmail.com>
2022-11-17 11:21:45 -0300

    desabilitando service por enquanto

--- backend/backend-products/src/main/java/com/guilhermeribeiro/service/ProductService.java
+++ backend/backend-products/src/main/java/com/guilhermeribeiro/service/ProductService.java
@@ -1,29 +1,9 @@
 package com.guilhermeribeiro.service;
 
-import com.guilhermeribeiro.model.Product;
-import com.guilhermeribeiro.model.ProductDTO;
-import com.guilhermeribeiro.repository.ProductRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class ProductService {
 
-    @Autowired
-    private ProductRepository productRepository;
 
-    public List<Product> productList() {
-        return productRepository.findAll();
-    }
-    public void createNewProduct(ProductDTO productDTO) {
-        Product product = new Product();
-
-        product.setName(productDTO.getName());
-        product.setReference(productDTO.getReference());
-        product.setPrice(productDTO.getPrice());
-
-        productRepository.save(product);
-    }
 }
